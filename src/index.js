0e58a10dfbd5a753d472b4dfd6d4422eb2c8612e
Fábio Duarte <fabio.ivi@gmail.com>
2023-10-25 20:52:23 -0400

    fix: add port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app } = require('./app');
 
 const port = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
 });
